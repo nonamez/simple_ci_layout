931a58c1c4e92f03400c66276b001ff34f0f2802
nonamez <nonamez123@gmail.com>
2013-09-29 17:39:33 +0300

    Update layout.php
    Some fixes.

--- layout.php
+++ layout.php
@@ -6,17 +6,19 @@
  * This class enables use of simple layouts in CodeIgniter
  *
  * @category	Libraries
- * @author	Kiril "NoNameZ" Calkin
- * @link	https://github.com/nonamez/simple_ci_layout
+ * @author		Kiril "NoNameZ" Calkin
+ * @link		https://github.com/nonamez/simple_ci_layout
  */
 
 class Layout{
 
 	private $_views;
 	private $_views_data;
+	private $_custom_views_data;
 	private $_CI;
 
-	function __construct(){
+	function __construct()
+	{
 		$this->_CI =& get_instance();
 
 		$this->_views = array(
@@ -32,106 +34,131 @@ class Layout{
 			'after'  => array(),
 			'bottom' => array()
 		);
+
+		$this->_custom_views_data = array();
 	}
 
-	public function load($views = FALSE, $view_data = NULL, $head_data = FALSE, $bottom_data = FALSE){
+	public function load($views = FALSE, $view_data = NULL, $head_data = FALSE, $bottom_data = FALSE)
+	{
 
-		if($head_data)
+		if ($head_data)
 			$this->setHeadData($head_data);
 
-		if($bottom_data)
+		if ($bottom_data)
 			$this->setBottomData($bottom_data);
 
-		if($this->_views['head'])
-			$this->_CI->load->view($this->_views['head'], $this->_views_data['head']);
+		if ($this->_views['head'])
+			$this->_showView($this->_views['head'], $this->_views_data['head']);
 
-		if(count($this->_views['before']) > 0)
-			foreach($this->_views['before'] as $view)
-				$this->_CI->load->view($view, $this->_views_data['before']);
+		if (count($this->_views['before']) > 0)
+			foreach ($this->_views['before'] as $view)
+				$this->_showView($view, $this->_views_data['before']);
 
-		if(is_array($views)){
-			foreach($views as $view)
-				$this->_CI->load->view($view, $view_data);
+		if (is_array($views)) {
+			foreach ($views as $view)
+				$this->_showView($view, $view_data);
 		}
 		else
-			$this->_CI->load->view($views, $view_data);
+			$this->_showView($views, $view_data);
 
-		if(count($this->_views['after']) > 0)
-			foreach($this->_views['after'] as $view)
-				$this->_CI->load->view($view, $this->_views_data['after']);
+		if (count($this->_views['after']) > 0)
+			foreach ($this->_views['after'] as $view)
+				$this->_showView($view, $this->_views_data['after']);
 
-		if($this->_views['bottom'])
-			$this->_CI->load->view($this->_views['bottom'], $this->_views_data['bottom']);
+		if ($this->_views['bottom'])
+			$this->_showView($this->_views['bottom'], $this->_views_data['bottom']);
 	}
 
-	public function setHeadData($data = FALSE){
-		if(is_array($data))
+	public function setHeadData($data = FALSE)
+	{
+		if (is_array($data))
 			$this->_views_data['head'] = array_merge($this->_views_data['head'], $data);
 	}
 
-	public function setBottomData($data = FALSE){
-		if(is_array($data))
+	public function setBottomData($data = FALSE)
+	{
+		if (is_array($data))
 			$this->_views_data['bottom'] = array_merge($this->_views_data['bottom'], $data);
 	}
 
-	public function setGlobalData($data = FALSE){
-		if(is_array($data)){
+	public function setGlobalData($data = FALSE)
+	{
+		if (is_array($data)) {
 			$this->setHeadData($data);
 			$this->setBottomData($data);
 
-			foreach($this->_views_data['before'] as &$value)
+			foreach ($this->_views_data['before'] as &$value)
 				$value = array_merge($value, $data);
 
-			foreach($this->_views_data['after'] as &$value)
+			foreach ($this->_views_data['after'] as &$value)
 				$value = array_merge($value, $data);
 		}
 	}
 
-	public function setDocumentHead($head = FALSE){
-		if($head)
+	public function setCustomData($view = FALSE, $data = FALSE)
+	{
+		if ($view && is_array($data))
+			$this->_custom_views_data[$view] = $data;
+	}
+
+	public function setDocumentHead($head = FALSE)
+	{
+		if ($head)
 			$this->_views['head'] = (string)$head;
 	}
 
-	public function setDocumentBottom($bottom = FALSE){
-		if($bottom)
+	public function setDocumentBottom($bottom = FALSE)
+	{
+		if ($bottom)
 			$this->_views['bottom'] = (string)$bottom;
 	}
 
-	public function setBeforeContent($before_content = FALSE){
-		if($before_content)
+	public function setBeforeContent($before_content = FALSE)
+	{
+		if ($before_content)
 			$this->_setView('before', (array)$before_content);
 	}
 
-	public function setAfterContent($after_content = FALSE){
-		if($after_content)
+	public function setAfterContent($after_content = FALSE)
+	{
+		if ($after_content)
 			$this->_setView('after', (array)$after_content);
 	}
 
-	private function _setView($condition, $views){
+	private function _setView($condition, $views)
+	{
 		// iteration over array of views
-		foreach($views as $key => $view_data){
+		foreach ($views as $key => $view_data) {
 
 			// checking if the view passed with or without data
-			if(is_array($view_data))
+			if (is_array($view_data))
 				$view =  $key;
-			else{
+			else {
 				$view = $view_data;
 				$view_data = FALSE;
 			}
 
-			if(!array_key_exists($view, $this->_views[$condition]))
+			if (!array_key_exists($view, $this->_views[$condition]))
 				// adding new view
 				array_push($this->_views[$condition], $view);
 
 			// adding view data if exists
-			if($view_data){
+			if ($view_data) {
 				// creating view data array if not exists
-				if(!isset($this->_views_data[$condition][$view]))
+				if (!isset($this->_views_data[$condition][$view]))
 					$this->_views_data[$condition][$view] = array();
 
 				$this->_views_data[$condition][$view] = array_merge($this->_views_data[$condition][$view], (array)$view_data);
 			}
 		}
+	}
+
+	private function _showView($view, $view_data)
+	{
+		if (isset($this->_custom_views_data[$view]))
+			$view_data = array_merge($view_data, $this->_custom_views_data[$view]);
+
+		$this->_CI->load->view($view, $view_data);
 	}
 }
 ?>
